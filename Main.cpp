146560522950b76059f6d7986d1b2eb200aeb30a
Timo Buitenhuis <timo.buitenhuis@coolblue.nl>
2020-12-26 14:54:44 +0100

    [EB] Added GLUT imports

--- Main.cpp
+++ Main.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
 #include <OpenGL/gl3.h>
+#include <GLUT/glut.h>
 
-int main(int, char**) {
-    glGetInteger64v(GL_MAJOR_VERSION, NULL);
+//Program to create an empty Widdow
+void init(){
+	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);	//Line C
+	glutInitWindowSize(640,480);
+	glutInitWindowPosition(1000,200);
+	glutCreateWindow("Simple Window");
+}
+
+void display()
+{
+	glClearColor(0.0,0.0,0.0,0.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glFlush();
+}
+
+int main(int argc,char **argv)
+{
     std::cout << "Hello, world!\n";
-
-    return 0;
+	glutInit(&argc,argv);
+	init();
+	glutDisplayFunc(display);
+	glutMainLoop();
+	
+	return 0;
 }
