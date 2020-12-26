000b938efc86dbe72b2948f5349a505fe04e6eb1
Timo Buitenhuis <timo.buitenhuis@coolblue.nl>
2020-12-26 17:15:56 +0100

    [EB] Drawing pixels to screen

--- Main.cpp
+++ Main.cpp
@@ -2,12 +2,26 @@
 #include <OpenGL/gl3.h>
 #include <GLUT/glut.h>
 
+int WIDTH = 400;
+int HEIGHT = 400;
+GLubyte* PixelBuffer = new GLubyte[WIDTH * HEIGHT * 3];
+
 //Program to create an empty Widdow
 void init(){
-	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);	//Line C
-	glutInitWindowSize(640,480);
+	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
+	glutInitWindowSize(WIDTH,HEIGHT);
 	glutInitWindowPosition(1000,200);
 	glutCreateWindow("Simple Window");
+}
+
+void drawPixel(int x, int y, int r, int g, int b, GLubyte* pixels, int width, int height)
+{
+    if (0 <= x && x < width && 0 <= y && y < height) {
+        int position = (x + y * width) * 3;
+        pixels[position] = r;
+        pixels[position + 1] = g;
+        pixels[position + 2] = b;
+    }
 }
 
 void display()
@@ -15,6 +29,8 @@ void display()
 	glClearColor(0.0,0.0,0.0,0.0);
 	glClear(GL_COLOR_BUFFER_BIT);
 	glFlush();
+    glDrawPixels(WIDTH, HEIGHT, GL_RGB, GL_UNSIGNED_BYTE, PixelBuffer);
+	glutSwapBuffers();
 }
 
 int main(int argc,char **argv)
@@ -22,6 +38,44 @@ int main(int argc,char **argv)
     std::cout << "Hello, world!\n";
 	glutInit(&argc,argv);
 	init();
+
+    drawPixel(50,150,255,255,255,PixelBuffer, WIDTH, HEIGHT);
+    drawPixel(51,150,255,255,255,PixelBuffer, WIDTH, HEIGHT);
+    drawPixel(52,150,255,255,255,PixelBuffer, WIDTH, HEIGHT);
+    drawPixel(53,150,255,255,255,PixelBuffer, WIDTH, HEIGHT);
+    drawPixel(54,150,255,255,255,PixelBuffer, WIDTH, HEIGHT);
+    drawPixel(55,150,255,255,255,PixelBuffer, WIDTH, HEIGHT);
+    drawPixel(50,151,255,255,255,PixelBuffer, WIDTH, HEIGHT);
+    drawPixel(51,151,255,255,255,PixelBuffer, WIDTH, HEIGHT);
+    drawPixel(52,151,255,255,255,PixelBuffer, WIDTH, HEIGHT);
+    drawPixel(53,151,255,255,255,PixelBuffer, WIDTH, HEIGHT);
+    drawPixel(54,151,255,255,255,PixelBuffer, WIDTH, HEIGHT);
+    drawPixel(55,151,255,255,255,PixelBuffer, WIDTH, HEIGHT);
+    drawPixel(50,152,255,255,255,PixelBuffer, WIDTH, HEIGHT);
+    drawPixel(51,152,255,255,255,PixelBuffer, WIDTH, HEIGHT);
+    drawPixel(52,152,255,255,255,PixelBuffer, WIDTH, HEIGHT);
+    drawPixel(53,152,255,255,255,PixelBuffer, WIDTH, HEIGHT);
+    drawPixel(54,152,255,255,255,PixelBuffer, WIDTH, HEIGHT);
+    drawPixel(55,152,255,255,255,PixelBuffer, WIDTH, HEIGHT);
+    drawPixel(50,153,255,255,255,PixelBuffer, WIDTH, HEIGHT);
+    drawPixel(51,153,255,255,255,PixelBuffer, WIDTH, HEIGHT);
+    drawPixel(52,153,255,255,255,PixelBuffer, WIDTH, HEIGHT);
+    drawPixel(53,153,255,255,255,PixelBuffer, WIDTH, HEIGHT);
+    drawPixel(54,153,255,255,255,PixelBuffer, WIDTH, HEIGHT);
+    drawPixel(55,153,255,255,255,PixelBuffer, WIDTH, HEIGHT);
+    drawPixel(50,154,255,255,255,PixelBuffer, WIDTH, HEIGHT);
+    drawPixel(51,154,255,255,255,PixelBuffer, WIDTH, HEIGHT);
+    drawPixel(52,154,255,255,255,PixelBuffer, WIDTH, HEIGHT);
+    drawPixel(53,154,255,255,255,PixelBuffer, WIDTH, HEIGHT);
+    drawPixel(54,154,255,255,255,PixelBuffer, WIDTH, HEIGHT);
+    drawPixel(55,154,255,255,255,PixelBuffer, WIDTH, HEIGHT);
+    drawPixel(50,155,255,255,255,PixelBuffer, WIDTH, HEIGHT);
+    drawPixel(51,155,255,255,255,PixelBuffer, WIDTH, HEIGHT);
+    drawPixel(52,155,255,255,255,PixelBuffer, WIDTH, HEIGHT);
+    drawPixel(53,155,255,255,255,PixelBuffer, WIDTH, HEIGHT);
+    drawPixel(54,155,255,255,255,PixelBuffer, WIDTH, HEIGHT);
+    drawPixel(55,155,255,255,255,PixelBuffer, WIDTH, HEIGHT);
+
 	glutDisplayFunc(display);
 	glutMainLoop();
 	
