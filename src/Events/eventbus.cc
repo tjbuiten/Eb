323ce4b0e9e2ec8a1e790527d5da4c4aec5548f8
Timo Buitenhuis <timo.buitenhuis@coolblue.nl>
2021-03-07 20:06:37 +0100

    [EB] Pass eventtype along properly

--- src/Events/eventbus.cc
+++ src/Events/eventbus.cc
@@ -4,11 +4,11 @@
 
 namespace events {
 void EventBus::Subscribe(EventType eventType, EventSubscriber* eventSubscriber) {
-    subscribers[EventType::ADD_SCREEN].push_back(eventSubscriber);
+    subscribers[eventType].push_back(eventSubscriber);
 }
 
 void EventBus::SentEvent(std::unique_ptr<events::Event> event) {
-    for (EventSubscriber* subscriber : subscribers[EventType::ADD_SCREEN]) {
+    for (EventSubscriber* subscriber : subscribers[event->GetType()]) {
         if (subscriber == nullptr) {
             subscribers[event->GetType()].remove(subscriber);
         }
